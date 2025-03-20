ccaeb96d8989d18554153a0b2877a9f31ba178c8
Jennifer Naasz <naasz.jl@gmail.com>
2025-03-20 09:38:26 -0400

    feat(polls): add templates

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls" # adds a namespace to the app for better url routing when multiple apps are involved
 urlpatterns = [
 	path("", views.index, name="index"),
+	path("<int:question_id>/", views.detail, name="detail"),
+	path("<int:question_id>/results/", views.results, name="results"),
+	path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
